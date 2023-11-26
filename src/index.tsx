c8ef3235d7499575b7c0bdff88b1aecf84b793ac
Grigor <artgrigapp@gmail.com>
2023-11-26 19:45:46 +0400

    Implement persist store

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { setupStore } from './redux/configureStore'
+import { store, persistor } from './redux/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
 import Todos from './pages/Todos'
 import './index.css'
 import 'swiper/css'
 
-const store = setupStore()
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Todos />} />
-      </Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Todos />} />
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 )
--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,12 +1,22 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import todosSlice from './todo/reducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todos'],
+}
 
 const rootReducer = combineReducers({
   todos: todosSlice,
 })
 
-export const setupStore = () =>
-  configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-  })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+})
+
+export const persistor = persistStore(store)
